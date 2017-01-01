20df14ecb51a51ff8186be299e32557291e622af
Carter Perrin <carterperrin@icloud.com>
2017-01-01 12:20:45 -0600

    Add files via upload

---
+++ ScrollRecipes.js
@@ -0,0 +1,125 @@
+import React, { Component } from 'react';
+import {
+  ScrollView
+} from 'react-native';
+
+var RecipeEntry = require('./RecipeEntry')
+
+export class ScrollRecipes extends Component {
+  render() {
+    return (
+      <ScrollView>
+        <RecipeEntry
+          recipeName = 'Artichoke Recipe Name'
+          recipeIngredients = '14'
+          recipePrice = '$9.99'
+          recipePrepTime = '30'
+          recipeImage = {require('./Resources/artichoke.jpg')}
+          location = 'Whole Foods Market'
+          locationDistance = '0.8 mi'
+        />
+        <RecipeEntry
+          recipeName = 'Artichoke Recipe Name'
+          recipeIngredients = '14'
+          recipePrice = '$9.99'
+          recipePrepTime = '30'
+          recipeImage = {require('./Resources/artichoke.jpg')}
+          location = 'Whole Foods Market'
+          locationDistance = '0.8 mi'
+        />
+        <RecipeEntry
+          recipeName = 'Artichoke Recipe Name'
+          recipeIngredients = '14'
+          recipePrice = '$9.99'
+          recipePrepTime = '30'
+          recipeImage = {require('./Resources/artichoke.jpg')}
+          location = 'Whole Foods Market'
+          locationDistance = '0.8 mi'
+        />
+        <RecipeEntry
+          recipeName = 'Artichoke Recipe Name'
+          recipeIngredients = '14'
+          recipePrice = '$9.99'
+          recipePrepTime = '30'
+          recipeImage = {require('./Resources/artichoke.jpg')}
+          location = 'Whole Foods Market'
+          locationDistance = '0.8 mi'
+        />
+        <RecipeEntry
+          recipeName = 'Artichoke Recipe Name'
+          recipeIngredients = '14'
+          recipePrice = '$9.99'
+          recipePrepTime = '30'
+          recipeImage = {require('./Resources/artichoke.jpg')}
+          location = 'Whole Foods Market'
+          locationDistance = '0.8 mi'
+        />
+        <RecipeEntry
+          recipeName = 'Artichoke Recipe Name'
+          recipeIngredients = '14'
+          recipePrice = '$9.99'
+          recipePrepTime = '30'
+          recipeImage = {require('./Resources/artichoke.jpg')}
+          location = 'Whole Foods Market'
+          locationDistance = '0.8 mi'
+        />
+        <RecipeEntry
+          recipeName = 'Artichoke Recipe Name'
+          recipeIngredients = '14'
+          recipePrice = '$9.99'
+          recipePrepTime = '30'
+          recipeImage = {require('./Resources/artichoke.jpg')}
+          location = 'Whole Foods Market'
+          locationDistance = '0.8 mi'
+        />
+        <RecipeEntry
+          recipeName = 'Artichoke Recipe Name'
+          recipeIngredients = '14'
+          recipePrice = '$9.99'
+          recipePrepTime = '30'
+          recipeImage = {require('./Resources/artichoke.jpg')}
+          location = 'Whole Foods Market'
+          locationDistance = '0.8 mi'
+        />
+        <RecipeEntry
+          recipeName = 'Artichoke Recipe Name'
+          recipeIngredients = '14'
+          recipePrice = '$9.99'
+          recipePrepTime = '30'
+          recipeImage = {require('./Resources/artichoke.jpg')}
+          location = 'Whole Foods Market'
+          locationDistance = '0.8 mi'
+        />
+        <RecipeEntry
+          recipeName = 'Artichoke Recipe Name'
+          recipeIngredients = '14'
+          recipePrice = '$9.99'
+          recipePrepTime = '30'
+          recipeImage = {require('./Resources/artichoke.jpg')}
+          location = 'Whole Foods Market'
+          locationDistance = '0.8 mi'
+        />
+        <RecipeEntry
+          recipeName = 'Artichoke Recipe Name'
+          recipeIngredients = '14'
+          recipePrice = '$9.99'
+          recipePrepTime = '30'
+          recipeImage = {require('./Resources/artichoke.jpg')}
+          location = 'Whole Foods Market'
+          locationDistance = '0.8 mi'
+        />
+        <RecipeEntry
+          recipeName = 'Artichoke Recipe Name'
+          recipeIngredients = '14'
+          recipePrice = '$9.99'
+          recipePrepTime = '30'
+          recipeImage = {require('./Resources/artichoke.jpg')}
+          location = 'Whole Foods Market'
+          locationDistance = '0.8 mi'
+        />
+      </ScrollView>
+    );
+  }
+}
+
+module.exports = ScrollRecipes;
